0352b54e429867b4556c2ce627ddf4b620f9317e
Izabela Pawlak <izabelapawlak87@gmail.com>
2020-02-22 15:58:17 +0100

    testowanie allegro269888

--- Allegro/src/test.java
+++ Allegro/src/test.java
@@ -2,10 +2,13 @@
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Init allegro project");
+        System.out.println("Init allegro project");//allegro
     }
 }
 //testowanie strony Allegro ponowne
 //tes1
+//testowanie ponowne
+
+
 
 
